58f855471260976e8f3dbdcd345b874f73b26398
KeharSr <keharojha56@gmail.com>
2024-05-10 21:32:10 +0545

    first

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Going to be a hacker in future
         </a>
       </header>
     </div>
